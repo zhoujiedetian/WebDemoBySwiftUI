b717702b13e058c361c2fbbdb95eba053ecba246
精灵要跳舞 <zhoujiedetian1@163.com>
2023-06-25 22:50:59 +0800

    TransitionDemo

--- TestUI/TestUI/OtherDemo/TransitionDemo/TransitionDemo.swift
+++ TestUI/TestUI/OtherDemo/TransitionDemo/TransitionDemo.swift
@@ -7,9 +7,179 @@
 
 import SwiftUI
 
-struct TransitionDemo: View {
+struct TransitionTopBarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(getCurrentDate())
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                Text("今天")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+            }
+            
+            Spacer()
+            
+            Image(uiImage: TransitionModel.sampleModels[0].image)
+                .resizable()
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(Color.gray, lineWidth: 1)
+                }
+        }
+    }
+    
+    func getCurrentDate(_ format: String = "EEEE, MMM d") -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: Date())
+    }
+}
+
+struct SummaryView: View {
+    let category: String
+    let headline: String
+    let subline: String
+    @Binding var isShowContent: Bool
+    var body: some View {
+        VStack {
+            Spacer()
+            Rectangle()
+                .frame(minHeight: 100, maxHeight: 150)
+                .overlay {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(self.category.uppercased())
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                            
+                            Text(self.headline.uppercased())
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(.primary)
+                                .minimumScaleFactor(0.1)
+                                .lineLimit(2)
+                                .padding(.bottom, 5)
+                            
+                            if !self.isShowContent {
+                                Text(self.subline)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                    .minimumScaleFactor(0.1)
+                                    .lineLimit(3)
+                            }
+                        }
+                        .padding()
+                        Spacer()
+                    }
+                }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct TansitionCardView: View {
+    let category: String
+    let headline: String
+    let subline: String
+    let image: UIImage
+    var content: String
+    @Binding var isShowContent: Bool
+    var body: some View {
+        GeometryReader { gr in
+            ZStack {
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Image(uiImage: self.image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: gr.size.width, height: 500)
+    //                        .padding(.horizontal, 15)
+                            .border(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), width: self.isShowContent ? 0 : 1)
+                            .cornerRadius(15)
+                            .overlay {
+                                SummaryView(category: self.category, headline: self.headline, subline: self.subline, isShowContent: self.$isShowContent)
+                                    .cornerRadius(self.isShowContent ? 0 : 15)
+                            }
+                        
+                        if self.isShowContent {
+                            Text(self.content)
+                                .foregroundColor(Color(.darkGray))
+                                .font(.system(.body, design: .rounded))
+                                .padding(.horizontal)
+                                .padding(.bottom, 50)
+                                .transition(.move(edge: .top))
+                                .animation(.linear, value: 0)
+                        }
+                    }
+                }
+                .shadow(color: Color(extRed: 64, extGreen: 64, extBlue: 64, opacity: 0.3), radius: self.isShowContent ? 0 : 15)
+                
+                if self.isShowContent {
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Button {
+                                self.isShowContent = false
+                            } label: {
+                                Image(systemName: "xmark.circle.fill")
+                                    .font(.system(size: 26))
+                                    .foregroundColor(.white)
+                                    .opacity(0.7)
+                            }
+
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, 50)
+                    .padding(.trailing)
+                }
+            }
+        }
+    }
+}
+
+struct TransitionDemo: View {
+    
+    @State private var showContents: [Bool] = Array(repeating: false, count: TransitionModel.sampleModels.count)
+    
+    enum ContentMode {
+        case list
+        case content
+    }
+    
+    private var contentMode: ContentMode {
+        self.showContents.contains(true) ? .content : .list
+    }
+    
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView {
+                VStack {
+                    TransitionTopBarView()
+                        .padding(.horizontal, 20)
+                        .opacity(self.contentMode == .content ? 0 : 1)
+                    
+                    
+                    ForEach(TransitionModel.sampleModels.indices, id: \.self) { index in
+                        GeometryReader { gr in
+                            TansitionCardView(category: TransitionModel.sampleModels[index].category, headline: TransitionModel.sampleModels[index].headline, subline: TransitionModel.sampleModels[index].subHeadline, image: TransitionModel.sampleModels[index].image, content: TransitionModel.sampleModels[index].content, isShowContent: self.$showContents[index])
+                                .offset(y: self.showContents[index] ? -gr.frame(in: .global).minY : 0)
+                                .padding(.horizontal, self.showContents[index] ? 0 : 20)
+                                .opacity(self.contentMode == .list || (self.contentMode == .content && self.showContents[index]) ? 1 : 0)
+                                .onTapGesture {
+                                    self.showContents[index] = true
+                                }
+                        }
+                        .frame(height: self.showContents[index] ? fullView.safeAreaInsets.top + fullView.safeAreaInsets.bottom + fullView.size.height : 500)
+                        
+                    }
+                }
+            }
+        }
     }
 }
 
--- TestUI/TestUI/OtherDemo/TransitionDemo/TransitionModel.swift
+++ TestUI/TestUI/OtherDemo/TransitionDemo/TransitionModel.swift
@@ -10,4 +10,14 @@ import SwiftUI
 struct TransitionModel: Identifiable {
     var id = UUID()
     var category: String
+    var headline: String
+    var subHeadline: String
+    var content: String
+    var image: UIImage
+    
+    static let sampleModels = [
+        TransitionModel(category: "推荐", headline: "编辑精选", subHeadline: "本周推荐语录", content: "要想在一个生活圈中生活下去，或者融入职场的氛围，首先你要学习这个圈子的文化和发展史，并尝试用这个圈子里面的话术和它们交流，这样才能顺利的融入这个圈子。", image: UIImage(named: "8697cf1ely8gashxj3b09j20u00u040v.jpg")!),
+        TransitionModel(category: "热榜", headline: "每日热榜", subHeadline: "今日热门事件推送", content: "要想在一个生活圈中生活下去，或者融入职场的氛围，首先你要学习这个圈子的文化和发展史，并尝试用这个圈子里面的话术和它们交流，这样才能顺利的融入这个圈子。", image: UIImage(named: "d0c21786ly1gavj2c0kcej20c8096dh7.jpg")!),
+        TransitionModel(category: "头条", headline: "精选头条", subHeadline: "你关注的才是头条", content: "要想在一个生活圈中生活下去，或者融入职场的氛围，首先你要学习这个圈子的文化和发展史，并尝试用这个圈子里面的话术和它们交流，这样才能顺利的融入这个圈子。", image: UIImage(named: "d0c21786ly1gavj2c0kcej20c8096dh7.jpg")!)
+    ]
 }
